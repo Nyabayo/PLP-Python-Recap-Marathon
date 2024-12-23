058a35d51a84fec53a787329c1706fed6ef1f6a4
Nyabayo <ernestosindo9@gmail.com>
2024-12-23 19:35:56 +0300

    introduction

---
+++ Week1/lesson1.py
@@ -0,0 +1,8 @@
+#Introduction to python language
+#Python is a high-level, interpreted programming language that is widely used for many different purposes, from web development to scientific computing to machine learning.
+#Why Python?
+# 1. Easy to learn
+# 2. Versatile: Python can be used for a wide range of apploications, including web development, machine learning, cyber security, data science, AI and more
+# 3. Larger and active community
+# 4. Plenty of libraries
+# 5. Cross-platform compatibility.
