85ba72094d671cf0f879b4d34da95ca58776751c
Nyabayo <ernestosindo9@gmail.com>
2024-12-23 20:25:32 +0300

    codechallenge1

---
+++ Week1/codechallenge1.py
@@ -0,0 +1,6 @@
+# . Personalized Greeting App 👋
+# Create a program that asks for the user’s name and favorite color, then prints a personalized greeting like: “Hello, [Name]! Your favorite color, [Color], is awesome!”
+
+name = input("Enter your name\n")
+color = input("Enter your favourite color\n")
+print(f"Hello, {name}!, your favourite color is {color}")
